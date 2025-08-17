00e007e2f1c3b386d749d5a0b6bed9b6a7190bd0
Masataka NOMOTO <nomoto@arstella.co.jp>
2025-08-17 01:29:40 +0000

    feat: Update RedmineCLI to v0.8.3
    Update SHA256 checksums for all platforms (osx-x64, osx-arm64, linux-x64, linux-arm64)

--- Formula/redmine.rb
+++ Formula/redmine.rb
@@ -1,26 +1,26 @@
 class Redmine < Formula
   desc "GitHub CLI-like tool for managing Redmine tickets"
   homepage "https://github.com/arstella-ltd/RedmineCLI"
-  version "0.8.2"
+  version "0.8.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/arstella-ltd/RedmineCLI/releases/download/v#{version}/redmine-cli-#{version}-osx-x64.zip"
-      sha256 "b34854ab0af6d381d28ef8818c257d1280d633739ee2437ffafa18f79cdcd7c2"
+      sha256 "4999bdbeb7df8e8de9757e7612f182b55f4ed1681d6ffc6d0e1c80b0e587192b"
     else
       url "https://github.com/arstella-ltd/RedmineCLI/releases/download/v#{version}/redmine-cli-#{version}-osx-arm64.zip"
-      sha256 "706a4d5efd312f5af736b36a775123bf87753fcd48df6dcf1e827c77aacf85a8"
+      sha256 "e37fbae91731a1f2c041aa34c5c0b998e5c8a20cb9bafb1019667e2f507fc8a2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/arstella-ltd/RedmineCLI/releases/download/v#{version}/redmine-cli-#{version}-linux-x64.zip"
-      sha256 "f835b25f0228405040c08926b7ec4b24db050f133f423d4a78a38abbf3155765"
+      sha256 "f96b00a4b6a25c3af342cf0e1c7058e8c43ae89649108a0f56f19152ee272127"
     else
       url "https://github.com/arstella-ltd/RedmineCLI/releases/download/v#{version}/redmine-cli-#{version}-linux-arm64.zip"
-      sha256 "3502535947544a70fc399b59ac91ae31f2916cf605e53caef71cba11684c1784"
+      sha256 "21a54aa760f7662510aa569ddcb6929c00fa57600101c2999f02ddb3e525562e"
     end
   end
 
